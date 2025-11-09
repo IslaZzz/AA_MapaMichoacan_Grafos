da0ac5fd5cb3a6079398d66106da9ea05a7aaa53
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2025-11-09 14:21:51 -0700

    Creación del proyecto
    Proyecto en Java con Maven

---
+++ Proyecto_Michoacan_Grafos/src/main/java/com/mycompany/proyecto_michoacan_grafos/Proyecto_Michoacan_Grafos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_michoacan_grafos;
+
+/**
+ *
+ * @author abrilislas
+ */
+public class Proyecto_Michoacan_Grafos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
